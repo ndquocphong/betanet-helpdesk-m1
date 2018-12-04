78047ed51ac5f2c6f9f7b49b524769202a53c495
ndquocphong <ndquocphong@gmail.com>
2018-12-04 19:37:00 +0700

    Implement Department Model

---
+++ Model/Resource/Department.php
@@ -0,0 +1,12 @@
+<?php
+
+class Betanet_HelpDesk_Model_Resource_Department extends Mage_Core_Model_Resource_Db_Abstract
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function _construct()
+    {
+        $this->_init('betanet_helpdesk/department', 'department_id');
+    }
+}
---
+++ sql/betanet_helpdesk_setup/uprade-1.1.0-1.2.0.php
@@ -0,0 +1,29 @@
+<?php
+
+/** @var Betanet_HelpDesk_Model_Resource_Setup $installer */
+$installer = $this;
+$installer->startSetup();
+$connection = $installer->getConnection();
+$departmentTableName = $installer->getTable('betanet_helpdesk/department');
+$departmentTable = $connection->newTable($departmentTableName)
+    ->addColumn('department_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, [
+        'unsigned' => true,
+        'nullable' => false,
+        'primary' => true,
+        'identity' => true,
+    ])
+    ->addColumn('title', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, [
+        'nullable' => false
+    ])
+    ->addColumn('enabled', Varien_Db_Ddl_Table::TYPE_BOOLEAN, null, [
+        'nullable' => false
+    ])
+    ->addColumn('role_view', Varien_Db_Ddl_Table::TYPE_BOOLEAN, null, [
+        'nullable' => false
+    ])
+    ->addIndex($installer->getIdxName($priorityTableName, ['title'], Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE), ['title'], [
+        'type' => Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE
+    ]);
+
+$connection->createTable($departmentTable);
+$installer->endSetup();
